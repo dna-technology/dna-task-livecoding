f65179c93e5d9b9d44cc3063cb33282ec962c369
dnaksiczek <karol.siczek@digitalnewagency.com>
2023-04-04 13:50:24 +0000

    Removed redundant fields

--- src/main/java/com/digitalnewagency/task/api/CovidCaseDto.java
+++ src/main/java/com/digitalnewagency/task/api/CovidCaseDto.java
@@ -3,8 +3,6 @@ package com.digitalnewagency.task.api;
 public class CovidCaseDto {
 
     private String userId;
-    private String latitude;
-    private String longitude;
 
     public String getUserId() {
         return userId;
@@ -12,21 +10,5 @@ public class CovidCaseDto {
 
     public void setUserId(String userId) {
         this.userId = userId;
-    }
-
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
     }
 }
